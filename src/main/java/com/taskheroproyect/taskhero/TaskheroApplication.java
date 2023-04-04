294605bf1a11213bd641d79ef743f82e0c58c2ac
Jhan Carlos Monroy Moros <jmonroym1003@gmail.com>
2023-04-03 23:16:33 -0500

    Creacion del proyecto...

---
+++ src/main/java/com/taskheroproyect/taskhero/TaskheroApplication.java
@@ -0,0 +1,13 @@
+package com.taskheroproyect.taskhero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskheroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskheroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/taskheroproyect/taskhero/TaskheroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskheroproyect.taskhero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskheroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
